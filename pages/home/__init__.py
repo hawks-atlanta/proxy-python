e5e5149fd2143b1adc5aeb34ce9c9b568521568e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-02 09:33:27 -0500

    revert: Restore default file

--- pages/home/__init__.py
+++ pages/home/__init__.py
@@ -1,16 +1,6 @@
 import flask
 from ._view import view
 
-def bad_function(): 
-        a = 1
-        b  =2 
-        return b
-
-
-
-
-
-
 
 home = flask.Blueprint("home", __name__)
 home.register_blueprint(view)
