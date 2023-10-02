209572864f6c69691939fa50ea793602eab23299
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-02 09:32:07 -0500

    ci: Introduce linter and formatter errors

--- pages/home/__init__.py
+++ pages/home/__init__.py
@@ -1,6 +1,16 @@
 import flask
 from ._view import view
 
+def bad_function(): 
+        a = 1
+        b  =2 
+        return b
+
+
+
+
+
+
 
 home = flask.Blueprint("home", __name__)
 home.register_blueprint(view)
