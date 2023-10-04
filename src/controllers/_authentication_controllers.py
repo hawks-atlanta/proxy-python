60f2b4d469a6610d98286413843e948e6314d41d
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-10-04 17:55:13 -0500

    refactor: Improve exception handling in login controller

--- src/controllers/_authentication_controllers.py
+++ src/controllers/_authentication_controllers.py
@@ -8,24 +8,24 @@ def login_handler():
         data = request.json
 
         if not data:
-            return {"message": "No JSON data provided in the request"}, 400
+            return {"msg": "No JSON data provided in the request"}, 400
 
         username = data.get("username")
         password = data.get("password")
 
         if not username or not password:
-            return {"message": "Required fields are missing in JSON data"}, 400
+            return {"msg": "Required fields are missing in JSON data"}, 400
 
         result = soap_client.service.auth_login(
             {"username": username, "password": password}
         )
 
         if result.auth is not None:
-            token = result.auth.token
-            return {"message": "Login successful", "token": token}, 200
-        else:
-            return {"message": "Invalid credentials"}, 401
+            jwt = result.auth.token
+            return {"msg": "Login successful", "jwt": jwt}, 200
+
+        return {"msg": "Invalid credentials"}, 401
 
     except Exception as e:
         print("SOAP Error:", str(e))
-        return {"message": "Internal error", "error": str(e)}, 500
+        return {"msg": "Internal error", "error": str(e)}, 500
