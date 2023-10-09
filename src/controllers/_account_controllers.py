38392d476c718eacc7e70e0bed98de08f2f1d186
Andrea Velasquez <55466005+andre154@users.noreply.github.com>
2023-10-09 14:26:41 -0500

    feat: Register endpoint (#39)
    * feat: enpoint registration coverage 57%
    
    Implementation of the registration endpoint, 57% coverage is achieved
    
    * style: Fix formatter and linter warnings
    
    * test: Use random data in register tests
    
    * refactor: Update register endpoint according to the `.net` spec
    
    * test: Fix broken test
    
    ---------
    
    Co-authored-by: Andvelavi <54145562+Andvelavi@users.noreply.github.com>
    Co-authored-by: Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>

---
+++ src/controllers/_account_controllers.py
@@ -0,0 +1,31 @@
+from flask import request
+from src.config.soap_client import soap_client
+
+
+def register_handler():
+    try:
+        # Get JSON data from the request
+        data = request.json
+
+        if not data:
+            return {"msg": "No JSON data provided in the request"}, 400
+
+        username = data.get("username")
+        password = data.get("password")
+
+        if not username or not password:
+            return {"msg": "Required fields are missing in JSON data"}, 400
+
+        result = soap_client.service.account_register(
+            {"username": username, "password": password}
+        )
+
+        if result.auth is not None:
+            jwt = result.auth.token
+            return {"msg": "Register succeeded", "token": jwt}, 200
+
+        return {"msg": "Username already registered"}, 409
+
+    except Exception as e:
+        print("[Exception] register_handler ->", str(e))
+        return {"msg": "Internal error", "error": str(e)}, 500
