2c3b24ce74e0041ffa7fd9c2637e5a8900262f5b
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-08 19:24:09 -0500

    refactor: Update login and refresh endpoints according to the spec

--- src/views/_authentication_views.py
+++ src/views/_authentication_views.py
@@ -5,12 +5,12 @@ from src.middlewares import auth_middlewares
 views = flask.Blueprint("authentication", __name__)
 
 
-@views.route("/auth_login", methods=["POST"])
+@views.route("/auth/login", methods=["POST"])
 def auth_login():
     return _authentication_controllers.login_handler()
 
 
-@views.route("/auth_refresh", methods=["POST"])
+@views.route("/auth/refresh", methods=["POST"])
 @auth_middlewares.token_required
 def auth_refresh(token):
     return _authentication_controllers.challenge_handler(token)
