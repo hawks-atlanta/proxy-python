990f3761f465e0470d4dca1b4f38283abcde3def
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-08 19:36:24 -0500

    style: Format file

--- src/middlewares/auth_middlewares.py
+++ src/middlewares/auth_middlewares.py
@@ -1,20 +1,18 @@
 from functools import wraps
 from flask import request, abort, jsonify, make_response
 
-def token_required(f): 
+
+def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         # Check the token is present as a header
         token = None
-        if 'Authorization' in request.headers:
-            token = request.headers['Authorization'].split("Bearer ")[1]
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split("Bearer ")[1]
         if not token:
-            abort(
-              make_response(
-                jsonify({"msg": "Token is missing"}), 401
-              )
-            )
-       
-       # Return the function with the token as a parameter
+            abort(make_response(jsonify({"msg": "Token is missing"}), 401))
+
+        # Return the function with the token as a parameter
         return f(token, *args, **kwargs)
+
     return decorated
