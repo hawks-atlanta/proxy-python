4671219b6e4647eb200e07c889dd368fe639f949
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-10-09 11:31:38 -0500

    feat: enable CORS for authentication on localhost

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import flask
+from flask_cors import CORS
 from src.views import views
 
 app = flask.Flask(__name__)
 app.register_blueprint(views)
 
+
+CORS(app, resources={r"/auth/*": {"origins": "http://localhost:5173"}})
+
 if __name__ == "__main__":
     app.run(debug=True)
+
