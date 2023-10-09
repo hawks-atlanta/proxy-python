1bc5dda9cf4d02c6e8c084ef8470fe4524b8f235
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-10-09 12:35:15 -0500

    feat: environment for CORS

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import flask
 from flask_cors import CORS
 from src.views import views
+from src.config.environment import variables
 
 app = flask.Flask(__name__)
 app.register_blueprint(views)
 
+allowed_origins_list = variables["ALLOWED_ORIGINS"].split(",")
 
-CORS(app, resources={r"/auth/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/*": {"origins": allowed_origins_list}})
 
 if __name__ == "__main__":
     app.run(debug=True)
