325be0344662a3f6688f1b7d7a39392b59859e56
Pedro Andrés Chaparro Quintero <62714297+PedroChaparro@users.noreply.github.com>
2023-10-10 19:12:58 -0500

    release: v0.0.11 (#45)
    * fix: Enable all CORS origins (#44)
    
    * chore: Remove origins environment variable
    
    * fix: Enable all origins in CORS config

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import flask
 from flask_cors import CORS
 from src.views import views
-from src.config.environment import variables
 
 app = flask.Flask(__name__)
 app.register_blueprint(views)
-
-allowed_origins_list = variables["ALLOWED_ORIGINS"].split(",")
-
-CORS(app, resources={r"/*": {"origins": allowed_origins_list}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/config/environment.py
+++ src/config/environment.py
@@ -2,5 +2,4 @@ import os
 
 variables = {
     "GATEWAY_BASEURL": os.getenv("GATEWAY_BASEURL", "http://localhost:8080"),
-    "ALLOWED_ORIGINS": os.getenv("ALLOWED_ORIGINS", "http://localhost:5173"),
 }
