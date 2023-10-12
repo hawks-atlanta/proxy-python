af9507083f7b2b5a833cb0ac50c865806778ca5d
Pedro Andrés Chaparro Quintero <62714297+PedroChaparro@users.noreply.github.com>
2023-10-12 06:07:21 -0500

    feat: Endpoint to get the file status (#49)
    * refactor: Remove exception from responses
    
    * ci: Remove unnecessary field in tagging pipeline
    
    * fix: Update metadata base url in docker-compose
    
    * feat(file): Endpoint to get the state of a file
    
    * test(file): Add tests to the endpoint to get file state
    
    * chore: Set current version

---
+++ src/views/_file_views.py
@@ -0,0 +1,12 @@
+import flask
+from src.middlewares import auth_middlewares
+from src.controllers import _file_controllers
+
+
+views = flask.Blueprint("file", __name__)
+
+
+@views.route("/file/<string:file_uuid>/status", methods=["GET"])
+@auth_middlewares.token_required
+def file_check(token, file_uuid):
+    return _file_controllers.check_state_handler(token, file_uuid)
