0cf73e4c45c1b1a6af68b62c0ba9ec60c263aed2
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-11 21:26:10 -0500

    refactor(file): Update status endpoint

--- src/views/_file_views.py
+++ src/views/_file_views.py
@@ -6,7 +6,7 @@ from src.controllers import _file_controllers
 views = flask.Blueprint("file", __name__)
 
 
-@views.route("/file/<string:file_uuid>/state", methods=["GET"])
+@views.route("/file/<string:file_uuid>/status", methods=["GET"])
 @auth_middlewares.token_required
 def file_check(token, file_uuid):
     return _file_controllers.check_state_handler(token, file_uuid)
