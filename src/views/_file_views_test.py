1f22f254cc217d2147f97e960aaa673dddecd9a8
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-13 17:40:56 -0500

    test(file): Add tests to the rename endpoint

--- src/views/_file_views_test.py
+++ src/views/_file_views_test.py
@@ -6,6 +6,7 @@ from config.soap_client import soap_client
 from src.lib.faker import fake_username, fake_password
 
 
+# GET STATUS TESTS
 get_status_test_data = {
     "username": fake_username(),
     "password": fake_password(),
@@ -88,64 +89,108 @@ def test_get_status_success_code():
     assert json_response["msg"] == "File status has been obtained successfully"
 
 
-def test_rename():
+# RENAME FILE TESTS
+rename_test_data = {
+    "username": fake_username(),
+    "password": fake_password(),
+    "file": {
+        "uuid": None,
+        "name": "test-file",
+        "content": randbytes(1024),
+    },
+}
+
+
+def test_rename_bad_request():
+    # Register an user
+    register_response = soap_client.service.account_register(
+        {
+            "username": rename_test_data["username"],
+            "password": rename_test_data["password"],
+        }
+    )
+    assert register_response.error is False
+
     # Login with the user
     login_response = soap_client.service.auth_login(
         {
-            "username": get_status_test_data["username"],
-            "password": get_status_test_data["password"],
+            "username": rename_test_data["username"],
+            "password": rename_test_data["password"],
         }
     )
     assert login_response.error is False
     token = login_response.auth.token
 
-    new_file_data = {
-        "name": "hello.txt",
-        "content": randbytes(1024),
-    }
+    # No JSON
+    response = app.test_client().patch(
+        f"/file/{rename_test_data['file']['uuid']}/rename",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 400
+
+    # Empty file name
+    response = app.test_client().patch(
+        f"/file/{rename_test_data['file']['uuid']}/rename",
+        json={"newName": ""},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 400
+
+
+def test_rename_success():
+    # Login with the user
+    login_response = soap_client.service.auth_login(
+        {
+            "username": rename_test_data["username"],
+            "password": rename_test_data["password"],
+        }
+    )
+    assert login_response.error is False
+    token = login_response.auth.token
+
+    # Upload a file
     create_file_response = soap_client.service.file_upload(
         {
             "token": token,
-            "fileName": new_file_data["name"],
-            "fileContent": new_file_data["content"],
+            "fileName": rename_test_data["file"]["name"],
+            "fileContent": rename_test_data["file"]["content"],
             "location": None,
         }
     )
     assert create_file_response.error is False
 
-    new_name = "new_file_name_slisth112333"
-    response = soap_client.service.file_rename(
-        {"token": token, "fileUUID": create_file_response.fileUUID, "newName": new_name}
+    # Rename the file
+    new_name = "renamed-test-file"
+    response = app.test_client().patch(
+        f"/file/{create_file_response.fileUUID}/rename",
+        json={"newName": new_name},
+        headers={"Authorization": f"Bearer {token}"},
     )
 
-    assert response.error is False
-    assert response.code == 204
-    assert "Rename successful" in response.msg
+    json_response = json.loads(response.data)
+    assert response.status_code == 200
+    assert json_response["msg"] == "The file has been renamed successfully"
+
+    # Update the test data
+    rename_test_data["file"]["uuid"] = create_file_response.fileUUID
+    rename_test_data["file"]["name"] = new_name
 
 
-def test_same_name():
+def test_rename_conflict():
     # Login with the user
     login_response = soap_client.service.auth_login(
         {
-            "username": get_status_test_data["username"],
-            "password": get_status_test_data["password"],
+            "username": rename_test_data["username"],
+            "password": rename_test_data["password"],
         }
     )
     assert login_response.error is False
     token = login_response.auth.token
 
-    new_file_data = {
-        "name": "test_file.txt",
-        "content": randbytes(1024),
-    }
-    create_file_response = soap_client.service.file_upload(
-        {
-            "token": token,
-            "fileName": new_file_data["name"],
-            "fileContent": new_file_data["content"],
-            "location": None,
-        }
+    # Try to rename the file with the same name
+    response = app.test_client().patch(
+        f"/file/{rename_test_data['file']['uuid']}/rename",
+        json={"newName": rename_test_data["file"]["name"]},
+        headers={"Authorization": f"Bearer {token}"},
     )
-    assert create_file_response.error is True
-    assert create_file_response.code == 409
-    assert "A file with the same name already exists" in create_file_response.msg
+    assert response.status_code == 409
