b1e93fc468187e15341161c3db1084559b64e93c
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-10-13 17:44:21 -0500

    feat: Endpoint to rename a file (#50)

--- src/controllers/_file_controllers.py
+++ src/controllers/_file_controllers.py
@@ -1,3 +1,5 @@
+import json
+from flask import request
 from src.config.soap_client import soap_client
 
 
@@ -18,3 +20,25 @@ def check_state_handler(token, file_uuid):
     except Exception as e:
         print("[Exception] check_state_handler ->", str(e))
         return {"msg": "There was an error checking the file state"}, 500
+
+
+def rename_handler(token, file_uuid):
+    try:
+        data = json.loads(request.data)
+        new_name = data["newName"]
+        if not new_name or len(new_name) == 0:
+            return {"msg": "New name is required"}, 400
+
+        request_data = {"token": token, "fileUUID": file_uuid, "newName": new_name}
+        response = soap_client.service.file_rename(request_data)
+
+        if response["error"] is True:
+            return {"msg": response["msg"]}, response["code"]
+        else:
+            return {"msg": "The file has been renamed successfully"}, 200
+
+    except ValueError:
+        return {"msg": "Invalid JSON data provided in the request"}, 400
+
+    except Exception:
+        return {"msg": "There was an error renaming the file"}, 500
