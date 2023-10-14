1e44e5c709090f6d29d113da8823e5c2c695314f
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-10-13 21:03:04 -0500

    test: create a folder

--- src/views/_file_views_test.py
+++ src/views/_file_views_test.py
@@ -194,3 +194,101 @@ def test_rename_conflict():
         headers={"Authorization": f"Bearer {token}"},
     )
     assert response.status_code == 409
+
+
+# CREATE FOLDER TEST
+test_folders = {
+    "username": fake_username(),
+    "password": fake_password(),
+    "folder": {
+        "directoryName": "test-directory232",
+        "location": "test-location232",
+        "token": "test-token232",
+    },
+}
+
+
+def test_createfolder_bad_request():
+    # Register an user
+    register_response = soap_client.service.account_register(
+        {
+            "username": test_folders["username"],
+            "password": test_folders["password"],
+        }
+    )
+    assert register_response.error is False
+
+    # Login with the user
+    login_response = soap_client.service.auth_login(
+        {
+            "username": test_folders["username"],
+            "password": test_folders["password"],
+        }
+    )
+    assert login_response.error is False
+    token = login_response.auth.token
+
+    # No JSON (400 Bad Request)
+    response = app.test_client().post(
+        "/folders",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 400
+
+    # Empty directory name (400 Bad Request)
+    response = app.test_client().post(
+        "/folders",
+        json={"directoryName": ""},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == 400
+
+
+def test_createfolder_success():
+    # Login with the user
+    login_response = soap_client.service.auth_login(
+        {
+            "username": test_folders["username"],
+            "password": test_folders["password"],
+        }
+    )
+    assert login_response.error is False
+    token = login_response.auth.token
+
+    # Create a folder
+    create_folder_response = app.test_client().post(
+        "/folders",
+        json={
+            "directoryName": test_folders["folder"]["directoryName"],
+            "location": test_folders["folder"]["location"],
+        },
+        headers={"Authorization": f"Bearer {token}"},
+    )
+
+    assert create_folder_response.status_code == 201
+
+
+def test_createfolder_duplicate():
+    # Login with the user
+    login_response = soap_client.service.auth_login(
+        {
+            "username": test_folders["username"],
+            "password": test_folders["password"],
+        }
+    )
+    assert login_response.error is False
+    token = login_response.auth.token
+
+    # Create a folder with a duplicate name
+    create_folder_response = app.test_client().post(
+        "/folders",
+        json={
+            "directoryName": test_folders["folder"]["directoryName"],
+            "location": test_folders["folder"]["location"],
+        },
+        headers={"Authorization": f"Bearer {token}"},
+    )
+
+    # already exists
+    assert create_folder_response.status_code == 409
+    json.loads(create_folder_response.data)
