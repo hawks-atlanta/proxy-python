2ade781061d0edee9e4482c117aefe124a288582
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-13 22:06:58 -0500

    refactor(auth): Create separated files for each request

--- src/controllers/authentication/login/_handler.py
+++ src/controllers/authentication/login/_handler.py
@@ -1,13 +1,17 @@
+import json
 from flask import request
 from src.config.soap_client import soap_client
 
 
 def login_handler():
     try:
-        data = request.json
+        data = json.loads(request.data)
         username = data.get("username")
         password = data.get("password")
-        if not username or not password:
+
+        not_valid_username = not username or len(username) == 0
+        not_valid_password = not password or len(password) == 0
+        if not_valid_username or not_valid_password:
             return {"msg": "Required fields are missing in JSON data"}, 400
 
         result = soap_client.service.auth_login(
@@ -20,23 +24,9 @@ def login_handler():
 
         return {"msg": "Invalid credentials"}, 401
 
+    except ValueError:
+        return {"msg": "Invalid JSON data provided in the request"}, 400
+
     except Exception as e:
         print("[Exception] login_handler ->", str(e))
         return {"msg": "There was an error logging in"}, 500
-
-
-def challenge_handler(token):
-    try:
-        response = soap_client.service.auth_refresh({"token": token})
-        if hasattr(response.auth, "token"):
-            refreshed_token = response.auth.token
-            return {
-                "msg": "JWT refreshed successfully",
-                "token": refreshed_token,
-            }, 200
-        else:
-            return {"msg": response.msg}, response.code
-
-    except Exception as e:
-        print("[Exception] challenge ->", str(e))
-        return {"msg": "There was an error validating or refreshing the session"}, 500
--- src/controllers/authentication/refresh/_refresh_test.py
+++ src/controllers/authentication/refresh/_refresh_test.py
@@ -1,45 +1,8 @@
 import json
 from main import app
-from config.soap_client import soap_client
+
+from src.config.soap_client import soap_client
 from src.lib.faker import fake_username, fake_password
-
-
-def test_auth_login_successful() -> None:
-    test_data = {"username": fake_username(), "password": fake_password()}
-
-    registration_data = {
-        "username": test_data["username"],
-        "password": test_data["password"],
-    }
-    registration_result = soap_client.service.account_register(registration_data)
-    assert registration_result.error is False
-
-    login_data = {"username": test_data["username"], "password": test_data["password"]}
-    response = app.test_client().post("/auth/login", json=login_data)
-    assert response.status_code == 200
-
-    json_response = json.loads(response.data)
-    assert json_response["msg"] != ""
-    assert json_response["token"] != ""
-
-
-def test_auth_login_invalid_credentials() -> None:
-    data = {"username": "unexistent", "password": "password"}
-    response = app.test_client().post("/auth/login", json=data)
-    json_response = json.loads(response.data)
-
-    assert response.status_code == 401
-    assert json_response["msg"] == "Invalid credentials"
-
-
-def test_auth_login_missing_fields() -> None:
-    data = {"username": "miguel"}
-    response = app.test_client().post("/auth/login", json=data)
-    assert response.status_code == 400
-
-    data = {"password": "miguel"}
-    response = app.test_client().post("/auth/login", json=data)
-    assert response.status_code == 400
 
 
 def test_auth_refresh_missing_token() -> None:
