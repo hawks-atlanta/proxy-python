6a44101813e9f556d72c3036e579a483378e8617
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-15 13:28:21 -0500

    fix(files): Update endpoint to create directories

--- src/controllers/files/create_directory/_handler.py
+++ src/controllers/files/create_directory/_handler.py
@@ -1,17 +1,21 @@
 import json
 from flask import request
 from src.config.soap_client import soap_client
+from src.lib.helpers import is_valid_uuid
 
 
 def create_new_dir_handler(token):
     try:
         data = json.loads(request.data)
-
         directoryName = data.get("directoryName")
         location = data.get("location")
 
-        if not directoryName or not location:
+        if not directoryName:
             return {"msg": "Required fields are missing in JSON data"}, 400
+
+        not_valid_location = location is not None and not is_valid_uuid(location)
+        if not_valid_location:
+            return {"msg": "Not valid location provided"}, 400
 
         response = soap_client.service.file_new_dir(
             {
