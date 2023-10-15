735542028e223ec113e1c025753ad788f6122963
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-15 13:53:10 -0500

    refactor(files): Update endpoint to list files

--- src/controllers/files/__init__.py
+++ src/controllers/files/__init__.py
@@ -2,12 +2,12 @@ from .check_file_status._handler import check_state_handler
 from .rename_file._handler import rename_handler
 from .create_directory._handler import create_new_dir_handler
 from .upload_file._handler import upload_file_handler
-from .list_files._handler import get_list_handler
+from .list_files._handler import list_files_handler
 
 FILES_HANDLERS = {
     "CHECK_STATE": check_state_handler,
     "RENAME": rename_handler,
     "CREATE_DIRECTORY": create_new_dir_handler,
     "UPLOAD": upload_file_handler,
-    "FILE_LIST": get_list_handler,
+    "FILE_LIST": list_files_handler,
 }
--- src/controllers/files/list_files/_handler.py
+++ src/controllers/files/list_files/_handler.py
@@ -1,19 +1,29 @@
 import json
 import flask
 from src.config.soap_client import soap_client
+from src.lib.helpers import is_valid_uuid
 
 
-def get_list_handler(token):
+def list_files_handler(token):
     try:
-        directory_uuid = flask.request.args.get("directoryUUID", "null")
+        directory_uuid = flask.request.args.get("directoryUUID", None)
+
+        # Validate directoryUUID
+        not_valid_directory_uuid = directory_uuid is not None and not is_valid_uuid(
+            directory_uuid
+        )
+
+        if not_valid_directory_uuid:
+            return {"msg": "Not valid directory UUID provided"}, 400
+
+        # Send the request
         request_data = {"token": token, "location": directory_uuid}
         response = soap_client.service.file_list(request_data)
 
-        print(response)
+        if response.error is True:
+            return {"error": response.msg}, response.code
 
-        if hasattr(response, "error") and response.error:
-            return json.dumps({"error": response.error})
-
+        # Parse files into dict
         files = [
             {
                 "name": file.name,
@@ -25,7 +35,10 @@ def get_list_handler(token):
             for file in response.files
         ]
 
-        response_data = {"files": files}
-        return json.dumps(response_data)
+        return {
+            "files": files,
+            "msg": "Files have been listed successfully",
+        }, 200
     except Exception as e:
-        return json.dumps({"error": str(e)})
+        print("[Exception] list_files_handler ->", e)
+        return {"msg": "There was an error listing the files"}, 500
