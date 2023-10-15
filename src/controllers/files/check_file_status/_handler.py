7c41c70445858a3e46e162d59692605375a2ec42
Pedro Andrés Chaparro Quintero <62714297+PedroChaparro@users.noreply.github.com>
2023-10-15 13:37:00 -0500

    fix: Add extra validations for UUIDs (#65)
    * fix(files): Update endpoint to create directories
    
    * fix(files): Add extra validations for UUIDs

--- src/controllers/files/check_file_status/_handler.py
+++ src/controllers/files/check_file_status/_handler.py
@@ -1,8 +1,13 @@
 from src.config.soap_client import soap_client
+from src.lib.helpers import is_valid_uuid
 
 
 def check_state_handler(token, file_uuid):
     try:
+        not_valid_file_uuid = not file_uuid or not is_valid_uuid(file_uuid)
+        if not_valid_file_uuid:
+            return {"msg": "Not valid file UUID provided"}, 400
+
         response = soap_client.service.file_check(
             {"token": token, "fileUUID": file_uuid}
         )
--- src/controllers/files/rename_file/_handler.py
+++ src/controllers/files/rename_file/_handler.py
@@ -1,14 +1,22 @@
 import json
 from flask import request
 from src.config.soap_client import soap_client
+from src.lib.helpers import is_valid_uuid
 
 
 def rename_handler(token, file_uuid):
     try:
         data = json.loads(request.data)
+
+        # Validate new name
         new_name = data["newName"]
         if not new_name or len(new_name) == 0:
             return {"msg": "New name is required"}, 400
+
+        # Validate file UUID
+        not_valid_file_uuid = not file_uuid or not is_valid_uuid(file_uuid)
+        if not_valid_file_uuid:
+            return {"msg": "Not valid file UUID provided"}, 400
 
         request_data = {"token": token, "fileUUID": file_uuid, "newName": new_name}
         response = soap_client.service.file_rename(request_data)
